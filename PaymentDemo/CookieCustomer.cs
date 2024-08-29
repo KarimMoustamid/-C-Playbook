c3aaf80bda0b747a8215c639239907ae184306fd
karimmoustamid <moustamid.teck@gmail.com>
2024-08-28 22:19:51 -0600

    Refactor Name property to include validation
    Converted Name property to use a private backing field and added setter validation to ensure that the name contains appropriate content. This change aims to improve data integrity by enforcing validation rules whenever the Name property is set.

--- PaymentDemo/CookieCustomer.cs
+++ PaymentDemo/CookieCustomer.cs
@@ -4,7 +4,19 @@ public class CookieCustomer
 {
 	public int Id { get; } // must be > 0
 
-	public string Name { get; } // must contain something
+	// public string Name { get; set; } // must contain something
+
+	private string _name;
+
+	public string Name
+	{
+		get => _name;
+		set
+		{
+			ValidateName(value, nameof(_name));
+			_name = value;
+		}
+	}
 
 	public string? Notes { get; set; }
 
