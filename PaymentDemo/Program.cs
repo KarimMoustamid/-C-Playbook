01e2180f848a7651ec1f699b684c1afb5979a97a
karimmoustamid <moustamid.teck@gmail.com>
2024-08-28 21:54:26 -0600

    Add PaymentDemo project to solution
    This commit integrates a new PaymentDemo project into the existing solution. It includes configuration files for .NET 8.0, necessary project metadata, and the CookieCustomer class with basic functionality. The Program.cs file initializes and demonstrates the use of the CookieCustomer class.

---
+++ PaymentDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Pluralsight.CShPlaybook.MethodsProps;
+CookieCustomer customer = new CookieCustomer(0, "", "");
+
+Console.WriteLine(customer);
+
+char firstCh = customer.NameFirstChar;
+Console.WriteLine("customer's name starts with " + firstCh);
